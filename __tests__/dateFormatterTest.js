c70b19d10f1e52d4349b574af6f3760ce0bda254
kbberker <kbbuk90@gmail.com>
2019-04-26 11:17:17 +0100

    Add test for date that is not today

--- __tests__/dateFormatterTest.js
+++ __tests__/dateFormatterTest.js
@@ -1,8 +1,15 @@
-import {format} from '../src/dateFormatter';
+import { format } from "../src/dateFormatter";
 
 it('formats today as "TODAY"', () => {
   const December = 11; //js Date object month is indexed from 0
   const systemDateTime = new Date(2018, December, 15, 10, 5).getTime();
   const dateTimeToFormat = new Date(2018, December, 15, 12, 50).getTime();
-  expect(format(dateTimeToFormat, systemDateTime)).toBe('TODAY');
+  expect(format(dateTimeToFormat, systemDateTime)).toBe("TODAY");
+});
+
+it('formats a date that is not today as "DD/MM/YYYY"', () => {
+  const December = 11;
+  const systemDateTime = new Date(2018, December, 15, 10, 5).getTime();
+  const dateTimeToFormat = new Date(2018, December, 16, 10, 5).getTime();
+  expect(format(dateTimeToFormat, systemDateTime)).toBe("16/12/2018");
 });
