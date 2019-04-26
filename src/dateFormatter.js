ab10f92b24bf7f96670fa7384d982d8813189d05
kbberker <kbbuk90@gmail.com>
2019-04-26 11:17:39 +0100

    Complete format function

--- src/dateFormatter.js
+++ src/dateFormatter.js
@@ -1,3 +1,11 @@
 export const format = (dateToFormatTimeMillis, systemDateTimeMillis) => {
-  return '';
+  const dateToFormat = new Date(dateToFormatTimeMillis);
+  const systemDate = new Date(systemDateTimeMillis);
+
+  if (dateToFormat.toDateString() === systemDate.toDateString()) {
+    return "TODAY";
+  } else {
+    // return dateToFormat.toLocaleString("en-GB");
+    return Intl.DateTimeFormat("en-GB").format(dateToFormat);
+  }
 };
